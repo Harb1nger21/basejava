06020e5fbbc8f1e4af2079dc7b2c0b1d71af88cf
Anton <harbinger2113@gmail.com>
2021-04-14 17:49:52 +0300

    реализация методов clear, save, get, delete, getAll, size для ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,56 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    static int countElements = 0;
 
     void clear() {
+        for (int i = 0; i < countElements; i++) {
+            storage[i] = null;
+        }
+        countElements = 0;
     }
 
     void save(Resume r) {
+        storage[countElements] = r;
+        countElements++;
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume forReturn = new Resume();
+        for (int i = 0; i < countElements; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                forReturn.uuid = uuid;
+                break;
+            }
+        }
+        return forReturn;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < countElements; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+        countElements--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] forReturn = new Resume[countElements];
+        int indexReturn = 0;
+        for (int i = 0; i <= countElements; i++) {
+            if (storage[i] != null) {
+                forReturn[indexReturn] = storage[i];
+                indexReturn++;
+            }
+        }
+        return forReturn;
     }
 
     int size() {
-        return 0;
+        return countElements;
     }
 }
