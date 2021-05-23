0acff076f447559fc75bc8ff11d9bf1cad8d1ca4
Anton <harbinger2113@gmail.com>
2021-05-23 19:50:22 +0300

    Приписал @Override к методам

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -6,11 +6,12 @@ import ru.javawebinar.basejava.model.Resume;
  * Array based storage for Resumes
  */
 public class ArrayStorage extends AbstractArrayStorage {
-
+    @Override
     protected void add(Resume resume, int index){
         storage[countElements] = resume;
     }
 
+    @Override
     protected int findIndex(String uuid) {
         for (int i = 0; i < countElements; i++) {
             if (storage[i].getUuid().equals(uuid)) {
--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -5,13 +5,14 @@ import ru.javawebinar.basejava.model.Resume;
 import java.util.Arrays;
 
 public class SortedArrayStorage extends AbstractArrayStorage {
-
+    @Override
     protected int findIndex(String uuid) {
         Resume searchKey = new Resume();
         searchKey.setUuid(uuid);
         return Arrays.binarySearch(storage, 0, countElements, searchKey);
     }
 
+    @Override
     protected void add(Resume resume, int index) {
         index = index * (-1) - 1;
         if (countElements + 1 < storage.length) {
