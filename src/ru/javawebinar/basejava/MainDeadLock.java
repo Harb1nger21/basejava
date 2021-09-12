508ab148f7283aa8a757ef89132c07a419b74891
Anton <harbinger2113@gmail.com>
2021-09-12 06:07:17 +0300

    правки MainDeadLock

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -6,26 +6,22 @@ public class MainDeadLock {
     private static final Thread firstThread = new Thread() {
         @Override
         public void run() {
-            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
             incForFirstThread();
-            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
         }
     };
 
     private static final Thread secondThread = new Thread(new Runnable() {
         @Override
         public void run() {
-            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
             incForSecondThread();
-            System.out.println(Thread.currentThread().getName() + ", " + Thread.currentThread().getState());
         }
     });
 
-    private static synchronized int incForFirstThread() {
+    private static int incForFirstThread() {
         return seven + incForSecondThread();
     }
 
-    private static synchronized int incForSecondThread() {
+    private static int incForSecondThread() {
         return five + incForFirstThread();
     }
 
