5bfda2b6cc6780ca8e71e56e32d56b029266452a
Anton <harbinger2113@gmail.com>
2021-09-16 15:00:41 +0300

    реализация DeadLock

--- src/ru/javawebinar/basejava/MainDeadLock.java
+++ src/ru/javawebinar/basejava/MainDeadLock.java
@@ -1,34 +1,27 @@
 package ru.javawebinar.basejava;
 
 public class MainDeadLock {
+    private static final String firstString = "первая строка";
+    private static final String secondString = "вторая строка";
+
+    private static final Thread firstThread = new Thread(() -> deadLock(firstString, secondString));
+    private static final Thread secondThread = new Thread(() -> deadLock(secondString, firstString));
+
     public static void main(String[] args) {
         firstThread.start();
         secondThread.start();
     }
 
-    private static String firstString = "первая строка";
-    private static String secondString = "вторая строка";
-
-    private static final Thread firstThread = new Thread() {
-        @Override
-        public void run() {
-            runFirstThread("новая");
+    private static void deadLock(String string1, String string2) {
+        synchronized (string1) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            synchronized (string2) {
+                System.out.println("deadlock");
+            }
         }
-    };
-    private static final Thread secondThread = new Thread(new Runnable() {
-        @Override
-        public void run() {
-            runSecondThread("обновленная");
-        }
-    });
-
-    private static synchronized void runFirstThread(String string) {
-        firstString = string + " " + firstString;
-        runSecondThread(string);
-    }
-
-    private static synchronized void runSecondThread(String string) {
-        secondString = string + " " + secondString;
-        runFirstThread(string);
     }
 }
