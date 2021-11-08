5869f007f32d70c365016903c3e73a62dec5cd78
Anton <harbinger2113@gmail.com>
2021-11-08 19:25:08 +0300

    Попытка реализации ResumeServletTable с отображением таблицы с резюме

--- src/ru/javawebinar/basejava/web/ResumeServletTable.java
+++ src/ru/javawebinar/basejava/web/ResumeServletTable.java
@@ -1,19 +1,34 @@
 package ru.javawebinar.basejava.web;
 
+import ru.javawebinar.basejava.model.Resume;
+import ru.javawebinar.basejava.sql.SqlHelper;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResumeServletTable extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        request.setCharacterEncoding("UTF-8");
-//        response.setCharacterEncoding("UTF-8");
-//        response.setContentType("text/html; charset=UTF-8");
+        SqlHelper helper = new SqlHelper(() -> DriverManager.getConnection("jdbc:postgresql://localhost:5432/resumes", "postgres", "Rjr2.ntqkm"));
+        List<Resume> resumes = new ArrayList<>();
+        helper.execute("SELECT uuid, fullname FROM resume", ps -> {
+                    ResultSet rs = ps.executeQuery();
+                    while (rs.next()) {
+                        resumes.add(new Resume(rs.getString("uuid"), rs.getString("full_name")));
+                    }
+                    return null;
+                }
+        );
+        request.setAttribute("resumes", resumes);
+        request.getRequestDispatcher("/tableOfResumes.jsp").forward(request, response);
 
-        request.getRequestDispatcher("../topjava/web/tableOfResumes.jsp").forward(request,response);
     }
 
     @Override
--- web/tableOfResumes.jsp
+++ web/tableOfResumes.jsp
@@ -1,5 +1,7 @@
+<%@ page import="ru.javawebinar.basejava.model.Resume" %>
+<%@ page import="java.util.List" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+"http://www.w3.org/TR/html4/loose.dtd">
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
@@ -11,23 +13,15 @@
         <td align="center">uuid</td>
         <td align="center">full name</td>
     </tr>
-    <tr>
-        <td>uuid1</td>
-        <td>Антон Рогожин</td>
-    </tr>
-    <tr>
-        <td>uuid2</td>
-        <td>Виктория Рогожина</td>
-    </tr>
-    <tr>
-        <td>uuid3</td>
-        <td>Екатерина Веселова</td>
-    </tr>
-    <tr>
-        <td>uuid1</td>
-        <td>Роман Веселов</td>
-    </tr>
+    <%
+        List<Resume> resumes = (List<Resume>) request.getAttribute("resumes");
+        for (Resume resume : resumes) {
+            out.println("<tr>");
+            out.println("<td align=\"right\">" + resume.getUuid() + "</td>");
+            out.println("<td align=\"right\">" + resume.getFullName() + "</td>");
+            out.println("</tr>");
+        }
+    %>
 </table>
-
 </body>
 </html>
