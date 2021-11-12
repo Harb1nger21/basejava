cbc497d7f85caa4809d11a48468472bd08a0b39a
Anton <harbinger2113@gmail.com>
2021-11-12 20:08:34 +0300

    Решил проблему с сервлетом прописав полный путь к файлу конфигурации

--- test/ru/javawebinar/basejava/storage/SqlStorageTest.java
+++ test/ru/javawebinar/basejava/storage/SqlStorageTest.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.storage;
 
 import ru.javawebinar.basejava.util.Config;
-import ru.javawebinar.basejava.storage.AbstractStorageTest;
 
 public class SqlStorageTest extends AbstractStorageTest {
 
