4aae8a06c88018e2e2898e15218e1e8ac3a47e2a
Anton <harbinger2113@gmail.com>
2021-11-16 10:15:19 +0300

    Добавил реализацию классов и jsp файлы из видео урока 16

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -43,8 +43,16 @@ public class Resume implements Comparable<Resume>, Serializable {
         return fullName;
     }
 
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
     public Map<ContactType, String> getContacts() {
         return contacts;
+    }
+
+    public String getContact(ContactType type) {
+        return contacts.get(type);
     }
 
     public Map<SectionType, AbstractSection> getSections() {
---
+++ web/WEB-INF/jsp/fragments/header.jsp
@@ -0,0 +1,10 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Victoriya
+  Date: 13.11.2021
+  Time: 20:46
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<header> <a href="resume">Управление резюме</a>
+</header>
