b2f947e65361a2f6e7dfbf783424a46dbfeb7bb9
Anton <harbinger2113@gmail.com>
2021-11-17 12:12:58 +0300

    Приведение проекта к виду из разбора ДЗ реализации таблицы(15)

--- src/ru/javawebinar/basejava/util/Config.java
+++ src/ru/javawebinar/basejava/util/Config.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("C:\\Users\\Victoriya\\Desktop\\Programming\\java\\topjava\\config\\resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config\\resumes.properties");
     private static final Config INSTANCE = new Config();
 
     private final Properties props = new Properties();
@@ -37,5 +37,14 @@ public class Config {
 
     public SqlStorage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
